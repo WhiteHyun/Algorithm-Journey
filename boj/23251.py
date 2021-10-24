c7d564e16385d17ff60866975ccb9cc893a85f2c
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-10-24 15:08:31 +0900

    23251 solved

---
+++ boj/23251.py
@@ -0,0 +1,10 @@
+# 23251번: 스물셋
+# https://www.acmicpc.net/problem/23251
+
+
+from sys import stdin
+input = stdin.readline
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        print(23*int(input()))
