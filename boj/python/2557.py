e373af6edaed24d3372dc6f6c03c610eab90fac4
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-11-20 14:50:41 +0900

    2557 solved (python)

---
+++ boj/python/2557.py
@@ -0,0 +1,11 @@
+# 2557번: Hello World
+# https://www.acmicpc.net/problem/2557
+# Version: Python 3.8.12
+
+
+from sys import stdin
+
+read = stdin.readline
+
+if __name__ == "__main__":
+    print("Hello World!")
