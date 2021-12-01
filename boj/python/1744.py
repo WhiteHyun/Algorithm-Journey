21b490b87caf909188f241df23dc3a27d788c8a3
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-12-01 23:21:21 +0900

    1744 solved (80ms -> 68ms)

--- boj/python/1744.py
+++ boj/python/1744.py
@@ -9,55 +9,36 @@ read = stdin.readline
 
 if __name__ == "__main__":
     count = int(read())
+    sequence = sorted([int(read()) for _ in range(count)])
+    maximum_value = 0  # 수열의 최대값
 
-    positive_sequence = []  # 양수
-    negative_sequence = []  # 음수
-    # 수열을 입력받음
-    for _ in range(count):
-        number = int(read())
-        positive_sequence.append(number) if number >= 0 else negative_sequence.append(
-            number
-        )
-    # == sort ==
-    positive_sequence.sort()
-    negative_sequence.sort()
-    positive_length = len(positive_sequence)
-    negative_length = len(negative_sequence)
-    result = 0
+    # 수열 중 자연수만 처리
+    while len(sequence) > 1 and sequence[-1] > 0:
+        positive_number1 = sequence.pop()
+        positive_number2 = sequence.pop()
 
-    # 작은 수부터 짝지어서 곱함
-    for i in range(0, negative_length - 1, 2):
-        result += negative_sequence[i] * negative_sequence[i + 1]
-
-    # 큰 수부터 짝지어 곱하되 만약 덧셈이 더 크면 더함 (ex: 1, 889)
-    for i in range(positive_length - 1, 0, -2):
-        if (mul_number := positive_sequence[i] * positive_sequence[i - 1]) < (
-            add_number := positive_sequence[i] + positive_sequence[i - 1]
-        ):
-            result += add_number
+        if positive_number2 > 0:  # 두 번째를 pop했을 때 음수이거나 0이 나올 수 있음
+            if (add_number := positive_number1 + positive_number2) < (
+                mul_number := positive_number1 * positive_number2
+            ):
+                maximum_value += mul_number
+            else:
+                maximum_value += add_number
         else:
-            result += mul_number
+            sequence.append(positive_number2)  # 도로 넣음
+            maximum_value += positive_number1
 
-    # 음수가 홀수개이고 0을 입력받았다면 음수는 생략 가능
-    if (
-        (negative_flag := negative_length & 1) == 1
-        and positive_sequence
-        and positive_sequence[0] == 0
-    ):
-        pass
-    # 만약 음수와 홀수 둘 다 홀수개일 경우 (짝짓지 못한 경우) 곱하거나 더하는 것중 가장 큰 값을 더함
-    elif (positive_flag := positive_length & 1) == 1 and negative_flag:
-        if (mul_number := positive_sequence[0] * negative_sequence[-1]) < (
-            add_number := positive_sequence[0] + negative_sequence[-1]
-        ):
-            result += add_number
-        else:
-            result += mul_number
-    # 음수만 홀수인 경우
-    elif negative_flag:
-        result += negative_sequence[-1]
-    # 양수만 홀수인 경우
-    elif positive_flag:
-        result += positive_sequence[0]
+    # 음수와 0을 포함하는 수열만 남았으므로 역정렬 수행
+    sequence.sort(reverse=True)
 
-    print(result)
+    while len(sequence) > 1:
+        negative_number1 = sequence.pop()
+        negative_number2 = sequence.pop()
+        maximum_value += negative_number1 * negative_number2
+
+    # 저장된 수열에는 원소가 하나만 있거나 없음
+    if sequence:
+        maximum_value += sequence[-1]
+
+    print(maximum_value)
+
