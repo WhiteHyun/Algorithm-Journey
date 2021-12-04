bed4e63ca7740f4818c468b76ef733cf4cada4ba
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-12-04 14:17:34 +0900

    5338 solved (python)

---
+++ boj/python/bronze5/5338.py
@@ -0,0 +1,17 @@
+# 5338번: 마이크로소프트 로고
+# https://www.acmicpc.net/problem/5338
+# Version: Python 3.9.7
+
+
+from sys import stdin
+
+read = stdin.readline
+
+if __name__ == "__main__":
+    print(
+        """       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|"""
+    )
