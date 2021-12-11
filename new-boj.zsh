13063695940670c115b0a602c1ba87fe2363172f
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-12-11 23:04:07 +0900

    자동으로 티어에 맞게 파일이 생성되도록 수정

--- new-boj.zsh
+++ new-boj.zsh
@@ -7,7 +7,7 @@
 # - 문제 번호를 이름으로 가지는
 # 파이썬 파일을 생성함.
 #
-# 사용법: ./new-boj.zsh [문제 번호] [난이도 티어] [제출 언어]
+# 사용법: ./new-boj.zsh [문제 번호] [제출 언어]
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
@@ -18,15 +18,52 @@ else
   problem_number="$1"
 fi
 
-# problem name
-if [ "$3" = "python" ]; then
-  solution_file="$DIR/boj/python/$2/$problem_number.py"
-elif [ "$3" = "swift" ]; then
-  solution_file="$DIR/boj/swift/$2/$problem_number.swift"
-fi
 
 problem_link="https://www.acmicpc.net/problem/$problem_number"
+tier_link="https://solved.ac/api/v3/problem/show?problemId=$problem_number"
+
 problem_name=$(curl -s -N "$problem_link" | sed -n "s/^.*<title>\(.*\)<\/title>.*$/\1/p")
+difficulty=$(curl -s -N "$tier_link" | jq ".level")
+
+# difficulty must be a number.
+if [ -n $difficulty ] && [ $difficulty -eq $difficulty ] 2>/dev/null; then
+    # OK. It is a number.
+else
+    echo "difficulty must be a number."
+    exit 1
+fi
+
+# 백준 문제 티어 가져오기
+if [ $((difficulty / 5)) -eq 0 ]; then
+    tier="bronze"
+elif [ $((difficulty / 5)) -eq 1 ]; then
+    tier="silver"
+elif [ $((difficulty / 5)) -eq 2 ]; then
+    tier="gold"
+elif [ $((difficulty / 5)) -eq 3 ]; then
+    tier="platinum"
+elif [ $((difficulty / 5)) -eq 4 ]; then
+    tier="diamond"
+fi
+
+if [ $((difficulty % 5)) -eq 1 ]; then
+    stage="5"
+elif [ $((difficulty % 5)) -eq 2 ]; then
+    stage="4"
+elif [ $((difficulty % 5)) -eq 3 ]; then
+    stage="3"
+elif [ $((difficulty % 5)) -eq 4 ]; then
+    stage="2"
+elif [ $((difficulty % 5)) -eq 0 ]; then
+    stage="1"
+fi
+
+difficulty_tier="$tier$stage"
+
+
+solution_file="$DIR/boj/python/$difficulty_tier/$problem_number.py"
+
+
 
 # python version
 python_version=$(python3 --version)
@@ -34,22 +71,15 @@ python_version=$(python3 --version)
 # today's date
 today=$(date "+%Y/%m/%d")
 
-# [제출 언어]가 파이썬인 경우
-if [ "$3" = "python" ]; then
-  echo "# $problem_name" >> "$solution_file"
-  echo "# $problem_link" >> "$solution_file"
-  echo "# Version: $python_version" >> "$solution_file"
-  echo -e "\n\nfrom sys import stdin\n\nread = stdin.readline\n\nif __name__ == \"__main__\":\n    pass" >> "$solution_file"
-# [제출 언어]가 swift인 경우
-elif [ "$3" = "swift" ]; then
-  echo "//" >> "$solution_file"
-  echo "//  $problem_name" >> "$solution_file"
-  echo "//  $problem_link" >> "$solution_file"
-  echo "//" >> "$solution_file"
-  echo "//  Created by WhiteHyun on $today." >> "$solution_file"
-  echo "//" >> "$solution_file"
-  echo -e "\nimport Foundation" >> "$solution_file"
-fi
+echo "#" >> "$solution_file"
+echo "#  $problem_name" >> "$solution_file"
+echo "#  $problem_link" >> "$solution_file"
+echo "#  Version: $python_version" >> "$solution_file"
+echo "#" >> "$solution_file"
+echo "#  Created by WhiteHyun on $today." >> "$solution_file"
+echo "#" >> "$solution_file"
+echo -e "\n\nfrom sys import stdin\n\nread = stdin.readline\n\nif __name__ == \"__main__\":\n    pass" >> "$solution_file"
+
 
 echo "$problem_name"
 echo "$solution_file"
