d49cbd2ee17438554958284eaf39b7672f51fb9e
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-12-14 22:30:25 +0900

    10171 solved

---
+++ boj/bronze5/10171.py
@@ -0,0 +1,17 @@
+#
+#  10171번: 고양이
+#  https://www.acmicpc.net/problem/10171
+#  Version: Python 3.9.7
+#
+#  Created by WhiteHyun on 2021/12/14.
+#
+
+
+if __name__ == "__main__":
+    print(
+        """\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|"""
+    )
+
