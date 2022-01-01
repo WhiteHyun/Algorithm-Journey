fbed2ed7196dc4c7bf53afd238fd4cf72885367a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-01-01 10:49:41 +0900

    solved 2558

---
+++ boj/bronze5/2558.py
@@ -0,0 +1,15 @@
+#
+#  2558번: A+B - 2
+#  https://www.acmicpc.net/problem/2558
+#  Version: Python 3.8.12
+#
+#  Created by WhiteHyun on 2022/01/01.
+#
+
+
+from sys import stdin
+
+read = stdin.readline
+
+if __name__ == "__main__":
+    print(int(read()) + int(read()))
