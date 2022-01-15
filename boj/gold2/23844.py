332191093346177a99cd2e94b9923df2725d1b23
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-01-15 22:30:16 +0900

    solved 23844

--- boj/gold2/23844.py
+++ boj/gold2/23844.py
@@ -7,64 +7,36 @@
 #
 
 
-from sys import stdin
-from collections import deque
+from sys import stdin, setrecursionlimit
 
 read = stdin.readline
-graph_dict = {1: []}  # 노드간 관계
+setrecursionlimit(100000)
+
+
+def dfs(node: int, level: int):
+    if level_count[level] < limit_count:
+        level_count[level] += 1
+
+    for child_node in graph_list[node]:
+        if not visited[child_node]:
+            visited[child_node] = True
+            dfs(child_node, level + 1)
 
 
 if __name__ == "__main__":
-    node_size, limit_node_count = map(int, read().split())
-    temp_queue = deque()
+    node_count, limit_count = map(int, read().split())
+    graph_list = [[] for _ in range(node_count + 1)]  # 1부터 노드 개수까지의 번호가 있음.
+    level_count = [0] * node_count
+    visited = [False] * (node_count + 1)  # 방문 노드 처리
 
-    # 입력받는 구간
-    for _ in range(node_size - 1):
+    # n-1만큼 입력을 받음
+    for _ in range(node_count - 1):
         node1, node2 = map(int, read().split())
-        if node1 == 1:
-            graph_dict[node1].append(node2)
-            graph_dict[node2] = []
-        elif node2 == 1:
-            graph_dict[node2].append(node1)
-            graph_dict[node1] = []
-        else:
-            temp_queue.append((node1, node2))
-        # 그래프 부모 관계 설정
-        temp_list = []
-        while temp_queue:
-            node1, node2 = temp_queue.pop()
-            if node1 in graph_dict:
-                graph_dict[node1].append(node2)
-                graph_dict[node2] = []
-            elif node2 in graph_dict:
-                graph_dict[node2].append(node1)
-                graph_dict[node1] = []
-            else:
-                temp_list.append((node1, node2))
-        temp_queue.extend(temp_list)
+        graph_list[node1].append(node2)
+        graph_list[node2].append(node1)
 
-    temp_queue.extend(graph_dict[1])
-    count = 1
+    # == DFS ==
+    visited[1] = True
+    dfs(1, 0)
 
-    while temp_queue:  # BFS - queue
-        if len(temp_queue) > limit_node_count:
-            # 노드간 DFS로 개수 셈
-            node_count_list = [0] * len(temp_queue)
-            for index, node in enumerate(temp_queue):
-                temp_stack = [node]
-                while temp_stack:
-                    temp_node = temp_stack.pop()
-                    temp_stack.extend(graph_dict[temp_node])
-                    node_count_list[index] += 1
-            # print(f"remove = {temp_queue[node_count_list.index(min(node_count_list))]}")
-            temp_queue.remove(temp_queue[node_count_list.index(min(node_count_list))])
-        # print(f"{temp_queue=}")
-        count += len(temp_queue)
-
-        temp_list = []  # BFS setting
-        for node in temp_queue:
-            temp_list.extend(graph_dict[node])
-
-        temp_queue = deque(temp_list)
-
-    print(count)
+    print(sum(level_count))
