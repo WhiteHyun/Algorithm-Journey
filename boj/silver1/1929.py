fe1f38bca116e6b5f0db64bf81abe1804fca2654
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-03-13 15:02:24 +0900

    solved 1929

--- boj/silver1/1929.py
+++ boj/silver1/1929.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     start, end = map(int, read().split())
     prime_number_list = [True for _ in range(end + 1)]
     prime_number_list[1] = False
-    for i in range(2, (start + end) >> 1):
+    for i in range(2, int(end ** 0.5) + 1):
         if not prime_number_list[i]:
             continue
-
-        for j in range(i << 1, end + 1, i):
+        # 소수의 제곱부터 바꿔나감 (이미 자신 이하의 소수 중에서 소인수되는 애들은 제외하여 성능 향상)
+        for j in range(i * i, end + 1, i):
             prime_number_list[j] = False
 
     for i in range(start, end + 1):
