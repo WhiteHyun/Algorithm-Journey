a41889b472ec8d1b0c200aca01e96a89171e1d75
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-16 12:31:04 +0900

    feat: Add solution for LeetCode problem 84 Largest Rectangle in Histogram

---
+++ swift/Algorithm/LeetCode/Hard/84. Largest Rectangle in Histogram.swift
@@ -0,0 +1,38 @@
+//
+//  84. Largest Rectangle in Histogram
+//  https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/16.
+//
+
+import Foundation
+
+final class LeetCodeSolution84 {
+  func largestRectangleArea(_ heights: [Int]) -> Int {
+    var answer = 0
+    var stack: [(index: Int, height: Int)] = []
+
+    for (index, height) in heights.enumerated() {
+      if stack.isEmpty || stack.last!.height < height {
+        stack.append((index, height))
+        continue
+      }
+
+      var start = index
+      while !stack.isEmpty && stack.last!.height > height {
+        let (i, h) = stack.popLast()!
+        start = i
+        answer = max(answer, h * (index - i))
+      }
+
+      stack.append((start, height))
+    }
+
+    for (index, height) in stack {
+      answer = max(answer, height * (heights.count - index))
+    }
+
+    return answer
+  }
+}
