d4620513287d6d344b2fd0dc251c701866c2b194
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-17 16:33:32 +0900

    feat: Add solution for LeetCode problem 55

---
+++ swift/Algorithm/LeetCode/Medium/55. Jump Game.swift
@@ -0,0 +1,21 @@
+//
+//  55. Jump Game
+//  https://leetcode.com/problems/jump-game/description/
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/17.
+//
+
+import Foundation
+
+final class LeetCodeSolution55 {
+  func canJump(_ numbers: [Int]) -> Bool {
+    var goal = numbers.endIndex - 1
+
+    for index in numbers.indices.dropLast().reversed() where index + numbers[index] >= goal {
+      goal = index
+    }
+
+    return goal == 0
+  }
+}
--- swift/Algorithm/main.swift
+++ swift/Algorithm/main.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-print(LeetCodeSolution1030().smallestFromLeaf([0, 1, 2, 3, 4, 3, 4]))
-print(LeetCodeSolution1030().smallestFromLeaf([25, 1, 3, 1, 3, 0, 2]))
-print(LeetCodeSolution1030().smallestFromLeaf([2, 2, 1, nil, 1, 0, nil, 0]))
-print(LeetCodeSolution1030().smallestFromLeaf([0, 1, 1, 0]))
+print(LeetCodeSolution55().canJump([2, 3, 1, 1, 4]))
+print(LeetCodeSolution55().canJump([3, 2, 1, 0, 4]))
+print(LeetCodeSolution55().canJump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]))
+print(LeetCodeSolution55().canJump([0, 2, 3]))
+print(LeetCodeSolution55().canJump([1, 0, 1, 0]))
+print(LeetCodeSolution55().canJump([2, 0, 1, 0]))
