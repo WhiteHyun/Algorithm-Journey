ddfee22d66105ca0c9c8bec0eb9474b1b0543650
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-18 10:33:44 +0900

    feat: Add solution for LeetCode problem 463

---
+++ swift/Algorithm/LeetCode/Easy/463. Island Perimeter.swift
@@ -0,0 +1,35 @@
+//
+//  463. Island Perimeter
+//  https://leetcode.com/problems/island-perimeter/description/
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/18.
+//
+
+import Foundation
+
+final class LeetCodeSolution463 {
+  func islandPerimeter(_ grid: [[Int]]) -> Int {
+    var perimeter = 0
+
+    let dx = [-1, 1, 0, 0]
+    let dy = [0, 0, -1, 1]
+
+    for i in grid.indices {
+      for j in grid[i].indices where grid[i][j] == 1 {
+        var surface = 0
+
+        for index in dx.indices {
+          let x = i + dx[index]
+          let y = j + dy[index]
+          if grid[safe: x]?[safe: y] == 1 {
+            surface += 1
+          }
+        }
+        perimeter += 4 - surface
+      }
+    }
+
+    return perimeter
+  }
+}
