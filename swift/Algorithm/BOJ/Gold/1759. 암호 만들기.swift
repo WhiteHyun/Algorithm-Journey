f838988d26f1a39391cfe5a7b974f2e88427aa69
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-18 12:01:04 +0900

    feat: Add solution for BOJ problem 1759

---
+++ swift/Algorithm/BOJ/Gold/1759. 암호 만들기.swift
@@ -0,0 +1,41 @@
+//
+//  1759. 암호 만들기
+//  https://www.acmicpc.net/problem/1759
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/18.
+//
+
+import Foundation
+
+final class BOJSolution1759 {
+  func solution() {
+    guard let count = readLine()?.components(separatedBy: .whitespaces).compactMap(Int.init).first,
+          let sortedArray = readLine()?.components(separatedBy: .whitespaces).sorted()
+    else {
+      return
+    }
+
+    var answerArray: [String] = .init(repeating: "", count: count)
+    var visited: [Bool] = .init(repeating: false, count: sortedArray.count)
+
+    func backTracking(variableCount: Int, flagIndex: Int) {
+      if variableCount == count {
+        let vowelsCount = answerArray.filter { $0 == "a" || $0 == "e" || $0 == "i" || $0 == "o" || $0 == "u" }.count
+        if vowelsCount > 0, answerArray.count - vowelsCount > 1 {
+          print(answerArray.joined())
+        }
+        return
+      }
+
+      for index in (flagIndex + 1) ..< sortedArray.count where visited[index] == false {
+        visited[index] = true
+        answerArray[variableCount] = sortedArray[index]
+        backTracking(variableCount: variableCount + 1, flagIndex: index)
+        visited[index] = false
+      }
+    }
+
+    backTracking(variableCount: 0, flagIndex: -1)
+  }
+}
--- swift/Algorithm/main.swift
+++ swift/Algorithm/main.swift
@@ -7,9 +7,4 @@
 
 import Foundation
 
-print(LeetCodeSolution55().canJump([2, 3, 1, 1, 4]))
-print(LeetCodeSolution55().canJump([3, 2, 1, 0, 4]))
-print(LeetCodeSolution55().canJump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]))
-print(LeetCodeSolution55().canJump([0, 2, 3]))
-print(LeetCodeSolution55().canJump([1, 0, 1, 0]))
-print(LeetCodeSolution55().canJump([2, 0, 1, 0]))
+BOJSolution1759().solution()
