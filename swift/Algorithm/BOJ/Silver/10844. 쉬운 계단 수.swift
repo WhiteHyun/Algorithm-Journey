c3876687dabf0c6a5dafc9c410269161d8fef43e
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-20 21:32:22 +0900

    feat: Add solution for BOJ problem 10844

---
+++ swift/Algorithm/BOJ/Silver/10844. 쉬운 계단 수.swift
@@ -0,0 +1,30 @@
+//
+//  10844. 쉬운 계단 수
+//  https://www.acmicpc.net/problem/1759
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/20.
+//
+
+import Foundation
+
+final class BOJSolution10844 {
+  func solution() {
+    guard let number = Int(readLine() ?? "")
+    else {
+      return
+    }
+    var dpArray: [[Int]] = .init(repeating: .init(repeating: 0, count: 10), count: number)
+    dpArray[0] = .init(repeating: 1, count: 10)
+    for index in dpArray.indices.dropFirst() {
+      dpArray[index][0] = dpArray[index - 1][1]
+      dpArray[index][9] = dpArray[index - 1][8]
+
+      for digit in 1 ... 8 {
+        dpArray[index][digit] = (dpArray[index - 1][digit - 1] + dpArray[index - 1][digit + 1]) % 1_000_000_000
+      }
+    }
+
+    print(dpArray.last!.dropFirst().reduce(0, +) % 1_000_000_000)
+  }
+}
--- swift/Algorithm/main.swift
+++ swift/Algorithm/main.swift
@@ -7,8 +7,4 @@
 
 import Foundation
 
-print(LeetCodeSolution2103().findFarmland([[1, 0, 0], [0, 1, 1], [0, 1, 1]]))
-print(LeetCodeSolution2103().findFarmland([[1, 1], [1, 1]]))
-print(LeetCodeSolution2103().findFarmland([[0]]))
-print(LeetCodeSolution2103().findFarmland([[1, 1, 0], [0, 0, 1], [0, 0, 0]]))
-print(LeetCodeSolution2103().findFarmland([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]))
+BOJSolution10844().solution()
