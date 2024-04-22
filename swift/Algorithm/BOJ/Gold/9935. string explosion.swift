e75b76f727c0786d2db3fbbf2c674ad0d58b6b07
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-22 16:22:47 +0900

    feat: Add solution for BOJ problem 9935

---
+++ swift/Algorithm/BOJ/Gold/9935. string explosion.swift
@@ -0,0 +1,25 @@
+//
+//  9935. string explosion
+//  https://www.acmicpc.net/problem/1759
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/04/22.
+//
+
+import Foundation
+
+final class BOJSolution9935 {
+  func solution(string: String, bomb: String) -> String {
+    var stack: [Character] = []
+    let bomb = ArraySlice(bomb)
+
+    for element in string {
+      stack.append(element)
+      if stack.count >= bomb.count, element == bomb.last, stack[(stack.endIndex - bomb.count)...] == bomb {
+        stack.removeLast(bomb.count)
+      }
+    }
+
+    return stack.isEmpty ? "FRULA" : String(stack)
+  }
+}
