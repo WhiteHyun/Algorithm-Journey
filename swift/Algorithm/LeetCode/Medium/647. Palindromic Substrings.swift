0259da864ec5683a87e3ee052e72641285b880dd
SeungHyun Hong <whi7ehyun@gmail.com>
2024-07-14 02:52:45 +0900

    feat: Add solution for LeetCode problem 647

---
+++ swift/Algorithm/LeetCode/Medium/647. Palindromic Substrings.swift
@@ -0,0 +1,39 @@
+//
+//  647. Palindromic Substrings.swift
+//  https://leetcode.com/problems/palindromic-substrings/description/
+//  Algorithm
+//
+//  Created by 홍승현 on 2024/07/13.
+//
+
+final class LeetCode647 {
+  func countSubstrings(_ s: String) -> Int {
+    let array = Array(s)
+    let n = array.count
+    var count = 0
+
+    var dp: [[Bool]] = .init(
+      repeating: .init(repeating: false, count: n),
+      count: n
+    )
+
+    for i in dp.indices {
+      dp[i][i] = true
+      count += 1
+    }
+
+    for i in dp.indices.dropLast() where array[i] == array[i + 1] {
+      dp[i][i + 1] = true
+      count += 1
+    }
+
+    for length in stride(from: 3, through: n, by: 1) {
+      for i in 0 ... n - length where array[i] == array[i + length - 1] && dp[i + 1][i + length - 2] {
+        dp[i][i + length - 1] = true
+        count += 1
+      }
+    }
+
+    return count
+  }
+}
---
+++ swift/AlgorithmTests/LeetCode/Medium/LeetCode647Tests.swift
@@ -0,0 +1,33 @@
+//
+//  LeetCode647Tests.swift
+//  AlgorithmTests
+//
+//  Created by 홍승현 on 2024/07/13.
+//
+
+import Testing
+
+@Suite(
+  "647. Palindromic Substrings",
+  .tags(.twoPointers, .string, .dynamicProgramming)
+)
+struct LeetCode647Tests {
+  private let problem = LeetCode647()
+
+  @Test
+  func example1() {
+    let result = problem.countSubstrings("abc")
+    #expect(result == 3, #"Expected '3', but got '\#(result)'. Three palindromic strings: "a", "b", "c"."#)
+  }
+
+  @Test
+  func example2() {
+    let result = problem.countSubstrings("aaa")
+    #expect(result == 6, #"Expected '6', but got '\#(result)'. Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa"."#)
+  }
+  @Test
+  func example3() {
+    let result = problem.countSubstrings("a")
+    #expect(result == 1, #"Expected '1', but got '\#(result)'"#)
+  }
+}
