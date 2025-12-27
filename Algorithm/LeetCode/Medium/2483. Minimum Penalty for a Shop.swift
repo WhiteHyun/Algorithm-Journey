9bd596bb0fd2d6a7bfa0c26ac5c192e0c58fef16
SeungHyun Hong <whi7ehyun@gmail.com>
2025-12-28 00:17:19 +0900

    [LeetCode] 2483. Minimum Penalty for a Shop 풀이 (#1)
    * feat(leetcode): add solution for problem 2483
    
    * chore: add PR template for algorithm solutions
    
    * refactor(2483): optimize space complexity from O(N) to O(1)
    
    - Remove prefix sum arrays (nCount, yCount)
    - Use single variable to track penalty while iterating
    
    * chore. fix typo

---
+++ Algorithm/LeetCode/Medium/2483. Minimum Penalty for a Shop.swift
@@ -0,0 +1,35 @@
+//
+//  2483. Minimum Penalty for a Shop.swift
+//  https://leetcode.com/problems/minimum-penalty-for-a-shop/description/
+//  Algorithm
+//
+//  Created by 홍승현 on 2025/12/26.
+//
+
+final class LeetCode2483 {
+  func bestClosingTime(_ customers: String) -> Int {
+    // 0시에 문을 닫을 경우 초기 패널티 계산
+    var currentPenalty = customers.count { $0 == "Y" }
+    var minimumPenalty = currentPenalty
+    var bestTime = 0
+
+    // 각 시간을 순차적으로 검사
+    for (index, element) in customers.enumerated() {
+      // 영업 시간을 1시간 연장할 경우:
+      // - customers[i]가 Y이면 (이제 영업 중이므로) 페널티 감소
+      // - customers[i]가 N이면 (불필요하게 가게를 열었으므로) 페널티 증가
+      if element == "Y" {
+        currentPenalty -= 1
+      } else {
+        currentPenalty += 1
+      }
+
+      if currentPenalty < minimumPenalty {
+        minimumPenalty = currentPenalty
+        bestTime = index + 1
+      }
+    }
+
+    return bestTime
+  }
+}
